9b5c7fb3f25bdad414339fb910ef020e38af8234
Mike Cooper <mikescooper@icloud.com>
2019-08-23 06:00:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,9 @@ def adding_matz
         }
      }
     programmer_hash.merge!(:yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# })
+ :known_for => "Ruby",
+:languages => ["LISP", "C"]
+ })
 
 programmer_hash
 end
